d5308ba5602db33a6296a2bf475408f1173f03df
Lu Gomes <lugomes.eq@gmail.com>
2020-10-10 10:25:09 -0400

    Factor out BookShelf component and arrange books acc to shelf attr

--- src/App.js
+++ src/App.js
@@ -3,19 +3,30 @@ import * as BooksAPI from "./BooksAPI";
 import "./App.css";
 import { Link, Route } from "react-router-dom";
 import SearchPage from "./SearchPage";
-import BookItem from "./BookItem";
+import BookShelf from "./BookShelf";
 
 class BooksApp extends React.Component {
   state = {
-    books: [],
+    allBooks: [],
+    shelves: { currentlyReading: [], wantToRead: [], read: [] },
   };
 
+  filterByShelf = (allBooks, shelf) => {
+    return allBooks.filter((book) => book.shelf === shelf);
+  };
   componentDidMount() {
-    BooksAPI.getAll()
-      .then((books) => this.setState({ books }))
-      .then(() => console.log(this.state.books));
+    BooksAPI.getAll().then((allBooks) => {
+      const currentlyReading = this.filterByShelf(allBooks, "currentlyReading");
+      const wantToRead = this.filterByShelf(allBooks, "wantToRead");
+      const read = this.filterByShelf(allBooks, "read");
+      this.setState({
+        allBooks,
+        shelves: { currentlyReading, wantToRead, read },
+      });
+    });
   }
   render() {
+    const { shelves } = this.state;
     return (
       <>
         <Route
@@ -27,54 +38,19 @@ class BooksApp extends React.Component {
                 <div className="list-books-title">
                   <h1>MyReads</h1>
                 </div>
-                {this.state.books.length && (
-                  <div className="list-books-content">
-                    <div>
-                      <div className="bookshelf">
-                        <h2 className="bookshelf-title">Currently Reading</h2>
-                        <div className="bookshelf-books">
-                          <ol className="books-grid">
-                            <li>
-                              <BookItem book={this.state.books[0]} />
-                            </li>
-                            <li>
-                              <BookItem book={this.state.books[1]} />
-                            </li>
-                          </ol>
-                        </div>
-                      </div>
-                      <div className="bookshelf">
-                        <h2 className="bookshelf-title">Want to Read</h2>
-                        <div className="bookshelf-books">
-                          <ol className="books-grid">
-                            <li>
-                              <BookItem book={this.state.books[2]} />
-                            </li>
-                            <li>
-                              <BookItem book={this.state.books[3]} />
-                            </li>
-                          </ol>
-                        </div>
-                      </div>
-                      <div className="bookshelf">
-                        <h2 className="bookshelf-title">Read</h2>
-                        <div className="bookshelf-books">
-                          <ol className="books-grid">
-                            <li>
-                              <BookItem book={this.state.books[4]} />
-                            </li>
-                            <li>
-                              <BookItem book={this.state.books[5]} />
-                            </li>
-                            <li>
-                              <BookItem book={this.state.books[6]} />
-                            </li>
-                          </ol>
-                        </div>
-                      </div>
-                    </div>
+                <div className="list-books-content">
+                  <div>
+                    <BookShelf
+                      title="Currently Reading"
+                      books={shelves.currentlyReading}
+                    />
+                    <BookShelf
+                      title="Want to Read"
+                      books={shelves.wantToRead}
+                    />
+                    <BookShelf title="Read" books={shelves.read} />
                   </div>
-                )}
+                </div>
                 <div className="open-search">
                   <Link to="/search">
                     <button>Add a book</button>
---
+++ src/BookShelf.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import "./App.css";
+import BookItem from "./BookItem";
+
+class BookShelf extends Component {
+  render() {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.title}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {this.props.books.map((book) => (
+              <li key={book.id}>
+                <BookItem book={book} />
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BookShelf;
